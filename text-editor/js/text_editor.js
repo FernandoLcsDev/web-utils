bb1dde5f861c6afe1b10ebf6030ca7ad3dddfcca
Fernando Lucas <fernandolcsdev@outlook.com>
2023-07-18 17:56:52 -0300

    Primeira versão do editor de texto

---
+++ text-editor/js/text_editor.js
@@ -0,0 +1,55 @@
+function setHeading() {
+    
+}
+
+function bold() {
+
+}
+
+function italic() {
+
+}
+
+function underline() {
+
+}
+
+function strikethrough() {
+
+}
+
+function superscript() {
+
+}
+
+function subscript() {
+
+}
+
+function alignLeft() {
+
+}
+
+function alignCenter() {
+
+}
+
+function alignRight() {
+
+}
+
+function alignJustify() {
+
+}
+
+function ordList() {
+
+}
+
+function unordList() {
+
+}
+
+function setColor() {
+
+}
